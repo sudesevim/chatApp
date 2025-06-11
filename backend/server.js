b09f65cdf2d682626defc4e04435dd01a03a2853
Sude Sevim <sudesevim0@gmail.com>
2025-06-11 15:04:16 +0300

    create user model

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
+const app = express();
+
+dotenv.config();
+const PORT = process.env.PORT || 5002;
+
+app.get("/", (req,res) => {
+    res.send("hellooo world")
+});
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+    connectToMongoDB();
+    console.log(`Server running on port ${PORT}`)
+});
