1991cfabd68fefa1c2202f9fcf9da5045a1d202a
Sude Sevim <sudesevim0@gmail.com>
2025-06-17 22:14:30 +0300

    add message routes

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
@@ -10,8 +11,10 @@ const PORT = process.env.PORT || 5002;
 dotenv.config();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => {
     connectToMongoDB();
