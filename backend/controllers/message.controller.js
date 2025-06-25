4e5de3382e28b5dda4d969674c26269d4ec24914
Sude Sevim <sudesevim0@gmail.com>
2025-06-25 11:29:27 +0300

    add zustand

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -41,22 +41,21 @@ export const sendMessage = async (req, res) => {
 };
 
 export const getMessages = async (req, res) => {
-    try {
-        const { id:userToChatId } = req.params;
-        const senderId = req.user._id;
-        const conversation = await Conversation.findOne({
-            participants: { $all: [senderId, userToChatId] },
-        }).populate("messages");
+	try {
+		const { id: userToChatId } = req.params;
+		const senderId = req.user._id;
 
-        if(!conversation) {
-            return res.status(200).json({});
-        }
+		const conversation = await Conversation.findOne({
+			participants: { $all: [senderId, userToChatId] },
+		}).populate("messages"); // NOT REFERENCE BUT ACTUAL MESSAGES
 
-        const messages = conversation.messages;
+		if (!conversation) return res.status(200).json([]);
 
-        res.status(200).json(messages);
-    } catch (error) {
-        console.log("Error in getMessages controller", error.message);
-        res.status(500).json({error: "Internal Server Error"});
-    }
-}
+		const messages = conversation.messages;
+
+		res.status(200).json(messages);
+	} catch (error) {
+		console.log("Error in getMessages controller: ", error.message);
+		res.status(500).json({ error: "Internal server error" });
+	}
+};
